5eacba86548c64dcf87d3147d027670477431a4d
Declan Davies <55316373+decodavies10@users.noreply.github.com>
2019-10-18 23:14:56 +0100

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzLibrary;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Buzzer_WhenDefault_ReturnsInput([Values(1,2, 4, 7, 8)]int input)
+        {
+            //Act
+            string Output = FizzBuzzer.GetValue(input);
+
+            //Assert
+            Assert.AreEqual(input.ToString(), Output);
+        }
+        [Test]
+        public void Buzzer_WhenDiv3_ReturnFizz([Values(3, 6, 9)] int input)
+        {
+            //Act
+            string Output = FizzBuzzer.GetValue(input);
+
+            //Assert
+            Assert.AreEqual("Fizz", Output);
+        }
+        [Test]
+        public void Buzzer_When5_ReturnBuzz([Values(5, 10)] int input)
+        {
+            //Act
+            string Output = FizzBuzzer.GetValue(input);
+
+            //Assert
+            Assert.AreEqual("Buzz", Output);
+        }
+        [Test]
+        public void Buzzer_WhenDiv3AndDiv5_ReturnFizzBuzz([Values(15)] int input)
+        {
+            //Act
+            string Output = FizzBuzzer.GetValue(input);
+
+            //Assert
+            Assert.AreEqual("FizzBuzz", Output);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzLibrary/FizzBuzzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            string output = string.Empty;
+            
+            if (input % 3 == 0)
+            {
+                output  += "Fizz";
+            }
+
+            if (input % 5 == 0)
+            {
+                output += "Buzz";
+            }
+
+            if (string.IsNullOrEmpty(output))
+            {
+                output = input.ToString();
+            }
+
+            return output;
+        }
+    }
+}
